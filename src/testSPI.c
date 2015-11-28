364ac182f9cede1fecb1996897a783236588ac52
José Fonseca <ee11126@fe.up.pt>
2015-11-28 12:11:17 +0000

    The working main file to test the Nokia5110 graphical library

--- src/testSPI.c
+++ src/testSPI.c
@@ -1,74 +1,15 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include "../include/lib_LCD_Nokia5110.h"
 #include <xc.h>
 
 #pragma config WDTE = OFF // This disables the watchdog timer!
-
-#define _XTAL_FREQ 4000000
-#define DC    LATC3
-#define SCE   LATC4
-#define RST   LATC5
-#define DIN   LATC2
-#define SCK   LATC0
 
 void TIMER2config (unsigned char comparePer, unsigned char preload,
                    unsigned char presc, unsigned char postsc) {
     PR2    = comparePer;  
     TMR2   = preload; 
     T2CON |= postsc | (presc<<3); 
-}
-
-void initLCD() {
-    // Some initial configs on port C
-    ANSELC &= 0x00; // All bits on port C are set to Digital I/O's
-    TRISC  &= 0x00; // All bits on Port C are set to Outputs
-    APFCON0 |= (1<<5); // Don't use special features on Pin RC3
-    APFCON1 |= (1<<2); // Don't use special features on Pin RC3
-    
-    LATC = 0x00;
-    
-    // All pins are initialy idle (HIGH), except for DIN
-    DC    = 1;
-    SCE   = 1;
-    RST   = 1;
-    DIN   = 0;
-    SCK   = 0;
-    
-    // Initial reset to the device
-    __delay_ms(300);
-    RST = 0;
-    __delay_ms(300);
-    RST = 1;
-}
-
-void sendByteLCD(unsigned char* buf, unsigned char dataCommand) {
-    DC  = dataCommand;
-    SCK = 0;
-    __delay_us(1);
-    SCE = 0;
-    __delay_us(1);
-    
-    for(int i=0; i < 8; i++) {
-        SCK = 0; // Clock goes idle
-        
-        // Since we send the MSB first, we mask bit 8 and we send its logical value
-        (*buf) & 0b10000000 ? DIN = 1 : DIN = 0;  
-        
-        // Rising Edge of the Clock
-        __delay_us(1);
-        SCK = 1;
-        __delay_us(1);
-        
-        // We shift the next bit
-        (*buf) = ((*buf)<<1);
-    }
-    
-    SCK = 0;
-}
-
-void printCharLCD(char c) {
-
 }
 
 void CLOCKconfig() {
@@ -103,9 +44,8 @@ void main(void) {
     __delay_ms(1000);
     
     while(1) {
-        buf = rand() % 256;
-        sendByteLCD(&buf, 1);
-        __delay_ms(1);
+        printCharLCD('T');
+        __delay_ms(1000);
     }
     
 }
