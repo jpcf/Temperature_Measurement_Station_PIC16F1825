caecdf1990b3702a1bd056365bed22d12b3079ae
jpcf <jose.pc.fonseca@gmail.com>
2015-12-02 23:31:38 +0000

    The first sketch of the final executable. Only missing the integration with OneWire readings

--- src/main.c
+++ src/main.c
@@ -1,33 +1,54 @@
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 #include <xc.h>
 #include "../include/PIC16F1825_configs.h"
 #include "../include/ds18b20.h"
+#include "../include/lib_LCD_Nokia5110.h"
+#include "../include/lib_memtest_PIC16F1825.h"
 
 int main(int argc, char** argv) {
-    uint8_t rom_test_result;
-    float tempe;
     
+    char tempStr[14];
+        
+    // ----- Device Initial Config ----- //
     pic16f1825_init();
     
-    /*
-     TESTE DA RAM
-     */
+    // ------ LCD Initial Config ------- //
+    initLCD();
+    configLCD();
+    clearLCD();
     
-    rom_test_result = memtest_program_mem();
-    if(0 == rom_test_result){
-        // METER NO LCD que deu merda xD   
+    // ---------- RAM Testing ---------- //
+    uint16_t RAMtest = memtest_MARCH_Cmin();
+    LATC1 = 1;
+    
+    // Returns 0x3000 if no Errors, otherwise returns the address of the first error
+    if(0x3000 == RAMtest){
+        sprintf(tempStr, "RAM Test OK!");  
+        printlnLCD(tempStr, strlen(tempStr), 2);
     }else {
-        // METER NO LCD ok!
+        sprintf(tempStr, "!RAM @ 0x%04X", RAMtest);
+        printlnLCD(tempStr, strlen(tempStr), 2);
     }
     
-    set_precision_ds18b20(PRECISION_12);
+    // ---------- ROM Testing ---------- //
+    uint8_t ROMtest = memtest_program_mem();
+    if(0 == ROMtest){
+        sprintf(tempStr, "ROM Test OK!");  
+        printlnLCD(tempStr, strlen(tempStr), 2); 
+    }else {
+        sprintf(tempStr, "ROM Error!");  
+        printlnLCD(tempStr, strlen(tempStr), 2);
+    }
+    
+    //set_precision_ds18b20(PRECISION_12);
     
     while(1){
-        start_temp_measure_ds18b20();
-        __delay_ms(1000);           // Wait for temperature measurement 
-        tempe = read_temp_ds18b20();
+        //start_temp_measure_ds18b20();
+        //__delay_ms(1000);           // Wait for temperature measurement 
+        //tempe = read_temp_ds18b20();
         
         // IMPRIMIR NO LCD O VALOR DA TEMPERATURA
   
